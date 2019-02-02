9ed7b362a48c5778d423c41dc0ed9a63b691fdfa
pfieffer <cbpmgr@gmail.com>
2019-02-02 18:19:50 +0545

    Add test methods to categories

--- app/src/test/java/com/mycompany/example/myapplication/SimpleMathTest.java
+++ app/src/test/java/com/mycompany/example/myapplication/SimpleMathTest.java
@@ -1,11 +1,15 @@
 package com.mycompany.example.myapplication;
 
+import com.mycompany.example.myapplication.categories.FastTests;
+import com.mycompany.example.myapplication.categories.SlowTests;
+
 import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class SimpleMathTest {
     SimpleMath sm;
@@ -26,11 +30,13 @@ public class SimpleMathTest {
         //System.out.println("Sending to Web service:  done executing test method in SimpleMathTest.");
     }
 
+    @Category(FastTests.class) //The testAdd() is now a part of FastTests Category
     @Test
     public void testAdd() {
         assertEquals("SimpleMath addition not adding correctly", 9, sm.add(4, 5));
     }
 
+    @Category(SlowTests.class) //The testDiff() is now a part of SlowTests Category
     @Test
     public void testDiff() {
         assertEquals("SimpleMath diff not subtracting correctly", 5, sm.diff(12, 7));
