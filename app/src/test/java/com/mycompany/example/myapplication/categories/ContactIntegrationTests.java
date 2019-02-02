69729a9df87dd40b2f21fe968a785590bc84d433
pfieffer <cbpmgr@gmail.com>
2019-02-02 18:26:33 +0545

    Setup for Instrumented Test

--- app/src/test/java/com/mycompany/example/myapplication/categories/ContactIntegrationTests.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.example.myapplication.categories;
-
-/*
-Categories are just logical groups of tests. They are helpful when the number of unit tests in application
-grows to a substantial amount
- */
-public class ContactIntegrationTests {
-}
--- app/src/test/java/com/mycompany/example/myapplication/categories/UserSignUpTests.java
+++
@@ -1,4 +0,0 @@
-package com.mycompany.example.myapplication.categories;
-
-public class UserSignUpTests {
-}
