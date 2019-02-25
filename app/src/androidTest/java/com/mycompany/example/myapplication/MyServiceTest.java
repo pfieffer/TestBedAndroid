0b2819e3a8f0c4c2de96e905a014514457b524a5
pfieffer <cbpmgr@gmail.com>
2019-02-25 17:55:26 +0545

    Simple test filtering

--- app/src/androidTest/java/com/mycompany/example/myapplication/MyServiceTest.java
+++ app/src/androidTest/java/com/mycompany/example/myapplication/MyServiceTest.java
@@ -3,6 +3,8 @@ package com.mycompany.example.myapplication;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.RequiresDevice;
+import android.support.test.filters.SdkSuppress;
 import android.support.test.rule.ServiceTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -22,11 +24,13 @@ public class MyServiceTest {
 
 
     @Test
+    @RequiresDevice //tells the framework to run this test only on a physical device and not an emulator
     public void testService() throws TimeoutException{
         myServiceTestRule.startService(new Intent(InstrumentationRegistry.getTargetContext(), MyService.class));
     }
 
     @Test
+    @SdkSuppress(minSdkVersion = 30) //tells the framework to run this test on the specified minSDK
     public void testBoundService() throws TimeoutException{
         IBinder binder = myServiceTestRule.bindService(new Intent(InstrumentationRegistry.getTargetContext(), MyService.class));
 
