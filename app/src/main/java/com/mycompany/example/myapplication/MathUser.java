fede6b1a52f4874ed834582021155fc3340acbf4
pfieffer <cbpmgr@gmail.com>
2019-02-26 19:14:06 +0545

    Setup Mockito and use it to mock objects and test.

---
+++ app/src/main/java/com/mycompany/example/myapplication/MathUser.java
@@ -0,0 +1,15 @@
+package com.mycompany.example.myapplication;
+
+
+public class MathUser {
+
+    private SimpleMath math;
+
+    public MathUser(SimpleMath math){
+        this.math = math;
+    }
+
+    public void doSomeMath(){
+        math.add(4,5);
+    }
+}
