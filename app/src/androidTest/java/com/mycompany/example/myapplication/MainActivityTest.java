e8497832871294d999168d038f09692bb4603cce
pfieffer <cbpmgr@gmail.com>
2019-02-26 18:32:01 +0545

    Add test on view with Espresso testing library

--- app/src/androidTest/java/com/mycompany/example/myapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/mycompany/example/myapplication/MainActivityTest.java
@@ -1,18 +1,20 @@
 package com.mycompany.example.myapplication;
 
-import android.os.RemoteException;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.UiObject;
-import android.support.test.uiautomator.UiObjectNotFoundException;
-import android.support.test.uiautomator.UiSelector;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -21,31 +23,27 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    @Ignore
-    public void testUiDevice() throws RemoteException {
-        UiDevice device = UiDevice.getInstance(
-                InstrumentationRegistry.getInstrumentation());
-        if (device.isScreenOn()) {
-            device.setOrientationLeft();
-            device.openNotification();
-        }
+    public void testEspresso() {
+        ViewInteraction interaction =
+                onView(allOf(withId(R.id.editText),
+                        withText("this is a test"),
+                        hasFocus()));
+        interaction.perform(replaceText("how about some new text"));
+
+        ViewInteraction interaction2 =
+                onView(allOf(withId(R.id.editText),
+                        withText("how about some new text")));
+        interaction2.check(matches(hasFocus()));
     }
 
+    //same thing done with method chaining
     @Test
-    public void testUiAutomatorAPI() throws UiObjectNotFoundException, InterruptedException {
-        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-
-        UiSelector editTextSelector = new UiSelector().className("android.widget.EditText").text("this is a test").focusable(true);
-        UiObject editTextWidget = device.findObject(editTextSelector);
-        editTextWidget.setText("this is new text");
-
-        Thread.sleep(2000);
-
-//        UiSelector buttonSelector = new UiSelector().className("android.widget.Button").text("Click Me").clickable(true);
-//        UiObject buttonWidget = device.findObject(buttonSelector);
-//        buttonWidget.click();
-//
-//        Thread.sleep(2000);
-
+    public void testEspressoSimplified() {
+        onView(allOf(withId(R.id.editText),
+                withText("this is a test"),
+                hasFocus())).perform(replaceText("how about some new text"));
+        onView(allOf(withId(R.id.editText),
+                withText("how about some new text"))).check(matches(hasFocus()));
     }
+
 }
